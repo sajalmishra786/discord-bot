84d0f718f54c7ca4b2c6a2c3f6ddaae424e7bf72
Rajat Rajput <109746630+rajatrajputdev@users.noreply.github.com>
2025-06-01 10:52:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 import discord
 from discord.ext import commands
-import os
+from os import getenv
+
 intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.command(name="ping")
 async def ping(ctx):
-    latency = bot.latency * 1000  # Convert to milliseconds
+    latency = bot.latency * 1000
     await ctx.send(f"🏓 Pong! Latency is {latency:.2f} ms")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}!")
 
-# Replace with your actual token
-bot.run(os.getenv("BOT_TOKEN"))
+bot.run(getenv("BOT_TOKEN"))
